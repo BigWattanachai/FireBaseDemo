e55abaa437c0ee235f1c296f5a9035718f4241bb
BiG.KMITL <templeboy1992@gmail.com>
2017-03-18 00:24:27 +0700

    - Create tab bar

---
+++ app/src/main/java/com/example/big/firebasedemo/fragment/PageOneFragment.java
@@ -0,0 +1,26 @@
+package com.example.big.firebasedemo.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.big.firebasedemo.R;
+
+
+public class PageOneFragment extends Fragment {
+    public PageOneFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page_one, container, false);
+    }
+}
